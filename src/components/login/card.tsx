49ec8c508a3700f85364e35f841a071911ed4859
Luis Alejandro Castillo Martínez <luiscasm2501@gmail.com>
2023-08-19 14:25:26 -0400

    FEATURE - LOGIN: - Added custom logo - Added login section with login form - Changed theme colors to custom palette - Changed landing page to login page - Changed button component to include loader

---
+++ src/components/login/card.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { LoginForm } from "./form";
+import { Logo } from "../ui/logo";
+
+export const LoginCard = () => {
+  const FormCard = (
+    <Card className="border-0  p-6 col-start-1 col-end-7">
+      <CardHeader className="mb-4">
+        <Logo className="mb-12" size={75} />
+        <CardTitle>
+          <div className="mb-2">Welcome to all things Rick and Morty.</div>
+        </CardTitle>
+        <CardDescription>
+          Log in to find all the info you need about everyone's favorite TV
+          show.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <LoginForm />
+      </CardContent>
+    </Card>
+  );
+  return (
+    <Card className="max-w-4xl mx-auto my-auto p-0 ">
+      <CardContent className="p-0">
+        <div className="flex grid-cols-10 sm:grid  border-0 relative">
+          {FormCard}
+
+          <div className="w-full relative sm:inline hidden col-start-7 col-end-11">
+            <Image
+              className="object-cover rounded-r "
+              src="/login/banner-2.jpg"
+              fill
+              alt="space banner"
+            />
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
