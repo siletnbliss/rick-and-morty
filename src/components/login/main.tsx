4d72edaeefcb8dd99493067b91e22afd3befd914
silentbliss <50888994+siletnbliss@users.noreply.github.com>
2023-08-20 11:38:44 -0400

    Revert "Feature/login"

--- src/components/login/main.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginCard } from "./card";
-
-export const LoginMain = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <LoginCard />
-    </div>
-  );
-};
